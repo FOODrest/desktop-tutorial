920c3c1d8d5677ce0c1b4788fa673d0600136c6a
Saad Rajeb <139350903+FOODrest@users.noreply.github.com>
2024-10-06 18:24:58 +0100

    seo2

--- freelance/src/App.jsx
+++ freelance/src/App.jsx
@@ -1,12 +1,19 @@
 import {React, useState } from 'react'
 import Header from './layout/Header'
 import Footer from './layout/footer'
+import { Helmet } from 'react-helmet';
 import Home from './layout/index/Home'
 
 function App() {
 
   return (
     <>
+      <Helmet>
+        <link rel="canonical" href="https://www.huggingtails.org/" />
+        <title>HuggingTails - Healthy Dog Meals & Walking Services</title>
+        <meta name="description" content="Discover nutritious meals and dog walking services tailored for your furry friend." />
+        <meta name="keywords" content="huggingtails,huggingtail,hugging tails,hugging tail, tail, chien, dog meals, dog walking, pet care, healthy dog food, HuggingTails" />
+      </Helmet>
       <Header/>
       <Home/>
       <Footer/>

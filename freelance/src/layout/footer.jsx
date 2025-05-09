9ff3f11445a2e32774bee935fbb95b4a29753ef7
Saad Rajeb <139350903+FOODrest@users.noreply.github.com>
2025-05-09 15:41:19 +0100

    Update footer.jsx

--- freelance/src/layout/footer.jsx
+++ freelance/src/layout/footer.jsx
@@ -63,7 +63,7 @@ const Footer = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:center">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">Hugging Tails
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://huggingtails.org/" className="hover:underline">Hugging Tails
             </a>. All Rights Reserved.
             </span>
         </div>
